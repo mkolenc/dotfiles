ad54c519b6425a1c77f01d194c1819466c76fa0e
Max Kolenc <maxkolenc@gmail.com>
2025-07-21 23:55:18 -0700

    Added line to call script 03

--- main.sh
+++ main.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+# Get the absolute path of the script directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/scripts" && pwd)"
+
 export UTILS="$SCRIPT_DIR/utils.sh"
 export DOWNLOAD_DIR="$SCRIPT_DIR/downloads"
 mkdir -p "$DOWNLOAD_DIR"
 
+# Run the installation
 bash "$SCRIPT_DIR/01_sanity_checks.sh"
 bash "$SCRIPT_DIR/02_download_installation_media.sh"
+# bash "$SCRIPT_DIR/03_prepare_filesystems.sh"
 
